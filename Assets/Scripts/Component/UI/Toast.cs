7f32457e6f78faf5153edb095e1c936118ea434c
박기용 <gemfile0@gmail.com>
2017-05-13 19:25:37 +0900

    Prevent the null pointer error that occurred by sequence when the performance of a game is slow.

---
+++ Assets/Scripts/Component/UI/Toast.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Toast: MonoBehaviour 
+{
+	public CanvasGroup panel;
+	public Text label;
+	Sequence sequence;
+
+	static Toast instance;
+
+	void Awake()
+	{
+		gameObject.SetActive(false);
+		instance = this;
+	}
+
+	public static void Show(string message, float duration)
+	{
+		instance.ShowMessage(message, duration);
+	}
+
+	public void ShowMessage(string message, float duration)
+	{
+		if (sequence != null && sequence.IsPlaying()) { return; }
+
+		sequence = DOTween.Sequence()
+			.OnStart(() => {
+				gameObject.SetActive(true);
+				panel.alpha = 0;
+				label.text = message;
+			})
+			.OnComplete(() => {
+				gameObject.SetActive(false);
+			});
+		sequence.Append(panel.DOFade(1, .295f).SetEase(Ease.OutCirc));
+		sequence.AppendInterval(duration);
+		sequence.Append(panel.DOFade(0, .295f).SetEase(Ease.InCirc));
+	}
+}
