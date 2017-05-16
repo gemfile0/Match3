8574e43e9ac3101476f77b8206bb4050e8700d96
박기용 <gemfile0@gmail.com>
2017-05-16 20:47:03 +0900

    Initial commit of making a custom editor for creating the level.

---
+++ Assets/Plugins/Gemfile0/Match3Editor/Scripts/Gem.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Gem: MonoBehaviour
+{
+	public int ID;
+}
---
+++ Assets/Plugins/Gemfile0/Match3Editor/Scripts/Level.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public class Level 
+{
+    public int[] gems;
+    public int[] tiles;
+    public int[] gravities;
+    public int moves;
+    public int cols;
+    public int rows;
+}
